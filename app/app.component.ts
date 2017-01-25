7e13e9b97fb837fc8631c3ba93150242b4106073
albaitas <internet622@gmail.com>
2017-01-25 18:34:17 +0200

    *ngFor ir *ngIf

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from "@angular/core";
 
+const todos = [
+    'Mokytis javaScript',
+    'Mokytis Angular 2',
+    'Sukurti svetainæ'
+];
+
 @Component({
     moduleId: module.id,
     selector: "app",
-    templateUrl: "app.component.html"
+    templateUrl: "app.component.html",
+    styleUrls: ["app.component.css"]
 
 })
 
 export class AppComponent {
-    title = "mano"
+    title = "Angular 2";
+    todos = todos;
 }
