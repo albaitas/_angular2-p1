a7d2ee1fade537592db300ffb9211b6d7f14c209
albaitas <internet622@gmail.com>
2017-01-27 20:11:39 +0200

    skaidome į tris komponentus: todo-form, todo-list, todo-item

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,24 +1,5 @@
 import { Component } from "@angular/core";
 
-class Todo{
-    constructor(public title: string,
-                public completed: boolean = false){}
-}
-const todos: Todo[] = [
-    {
-        title: "Mokytis JavaScript",
-        completed: true
-    },
-    {
-        title: "Mokytis Angular 2",
-        completed: false
-    },
-    {
-        title: "Sukurti svetaine",
-        completed: false
-    }
-];
-
 @Component({
     moduleId: module.id,
     selector: "app",
@@ -29,25 +10,5 @@ const todos: Todo[] = [
 
 export class AppComponent {
     title: string = "Angular 2";
-    todos: Todo[] = todos;
-    newTodoTitle: string = '';
 
-    create(){
-      let todo: Todo = new Todo(this.newTodoTitle);
-
-        this.todos.push(todo);
-        this.newTodoTitle = '';
-
-    }
-
-    toggle(todo: Todo){
-        todo.completed = !todo.completed;
-    }
-
-    delete(todo: Todo){
-        let index = this.todos.indexOf(todo);
-        if(index > -1){
-            this.todos.splice(index, 1);
-        }
-    }
 }
---
+++ app/shared/data.ts
@@ -0,0 +1,14 @@
+export const todos = [
+    {
+        title: "Mokytis JavaScript",
+        completed: true
+    },
+    {
+        title: "Mokytis Angular 2",
+        completed: false
+    },
+    {
+        title: "Sukurti svetaine",
+        completed: false
+    }
+];
